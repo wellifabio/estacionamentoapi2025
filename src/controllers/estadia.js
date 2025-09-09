10a2de41e26052f093cb53cd27d939407acad267
wellifabio <wellifabio@hotmail.com>
2025-09-09 15:16:27 -0300

    corrigida rota hoje

--- src/controllers/estadia.js
+++ src/controllers/estadia.js
@@ -22,7 +22,7 @@ const readToday = async (req, res) => {
     try {
         const estadias = await prisma.estadia.findMany({
             where: {
-                createdAt: {
+                entrada: {
                     gte: new Date(new Date().setHours(0, 0, 0, 0)),
                     lt: new Date(new Date().setHours(23, 59, 59, 999))
                 }
